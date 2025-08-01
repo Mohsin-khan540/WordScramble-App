7adb701bbe14089ab422859cd7744ba06a663e4f
Mohsin-khan540 <arhamk4555@gmail.com>
2025-08-01 14:40:46 +0500

    Initial Commit

---
+++ wordScramble/wordScrambleApp.swift
@@ -0,0 +1,17 @@
+//
+//  wordScrambleApp.swift
+//  wordScramble
+//
+//  Created by Mohsin khan on 01/08/2025.
+//
+
+import SwiftUI
+
+@main
+struct wordScrambleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ wordScrambleTests/wordScrambleTests.swift
@@ -0,0 +1,17 @@
+//
+//  wordScrambleTests.swift
+//  wordScrambleTests
+//
+//  Created by Mohsin khan on 01/08/2025.
+//
+
+import Testing
+@testable import wordScramble
+
+struct wordScrambleTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
